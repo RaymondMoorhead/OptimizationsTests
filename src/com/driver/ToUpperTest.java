e9d659d4be8359faa0175403ef45300521557a7e
Raymond <raymond.moorhead@gmail.com>
2020-07-21 08:03:46 -0700

    Finished bugfixing of ToUpper methods and tests found speed increase of about 89.7% with branchless method

--- src/com/driver/ToUpperTest.java
+++ src/com/driver/ToUpperTest.java
@@ -4,57 +4,66 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Random;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Rule;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 import com.testfunc.ToUpper;
 
 class ToUpperTest {
 	
     private StringBuilder[] words;
-    private int samples = 10000;
+    private int samples = 1000000;
     private int wordLength = 10;
-    
+    private long startTime;
 
-    @Before
+    @BeforeEach
     public void setUp() {
     	words = new StringBuilder[samples];
     	Random rand = new Random(0);
     	int letterRange = (int)'z' - (int)'a';
     	
-    	
         for(int i = 0; i < samples; ++i) {
         	words[i] = new StringBuilder(wordLength);
+        	words[i].setLength(wordLength);
         	for(int j = 0; j < wordLength; ++j)
         		// pick a letter a-z, then pick lower or upper case
         		words[i].setCharAt(j, (char)(rand.nextInt(letterRange) + (int)(rand.nextBoolean() ? 'a' : 'A')));
         }
+        startTime = System.currentTimeMillis();
     }
 
-    @After
-    public void tearDown() {
+    @AfterEach
+    public void tearDown(TestInfo testInfo) {
+    	startTime = System.currentTimeMillis() - startTime;
+    	System.out.println("Test " + testInfo.getDisplayName() + " has run in " + startTime + " milliseconds");
     	words = null;
     }
     
 	@Test
+	@DisplayName("testBasicFunctionality")
 	void testBasicFunctionality() {
 		
 		for(char i = 'a'; i < 'z'; ++i) {
-			assertEquals(i + 32, ToUpper.Basic(i));
-			assertEquals(i, ToUpper.Basic((char)(i + 32)));
+			assertEquals(i - ToUpper.lowerUpperGap, ToUpper.Basic(i));
+			assertEquals(i - ToUpper.lowerUpperGap, ToUpper.Basic((char)(i - ToUpper.lowerUpperGap)));
 		}
 	}
 	
 	@Test
+	@DisplayName("testBranchlessFunctionality")
 	void testBranchlessFunctionality() {
 		for(char i = 'a'; i < 'z'; ++i) {
-			assertEquals(i + 32, ToUpper.Branchless(i));
-			assertEquals(i, ToUpper.Branchless((char)(i + 32)));
+			assertEquals(i - ToUpper.lowerUpperGap, ToUpper.Branchless(i));
+			assertEquals(i - ToUpper.lowerUpperGap, ToUpper.Branchless((char)(i - ToUpper.lowerUpperGap)));
 		}
 	}
 
 	@Test
+	@DisplayName("testBasicSpeed")
 	void testBasicSpeed() {
 		for(int i = 0; i < words.length; ++i) {
 			for(int j = 0; j < words[i].length(); ++j)
@@ -63,6 +72,7 @@ class ToUpperTest {
 	}
 	
 	@Test
+	@DisplayName("testBranchlessSpeed")
 	void testBranchlessSpeed() {
 		for(int i = 0; i < words.length; ++i) {
 			for(int j = 0; j < words[i].length(); ++j)
--- src/com/testfunc/ToUpper.java
+++ src/com/testfunc/ToUpper.java
@@ -2,16 +2,18 @@ package com.testfunc;
 
 public class ToUpper {
 	
+	public static final char lowerUpperGap = 'a' - 'A';
+	
 	public static char Basic(char data) {
 		if(data >= 'a' && data <= 'z')
-			data += 'A' - 'a';
+			data -= lowerUpperGap;
 		return data;
 	}
 	
 	public static char Branchless(char data) {
-		// gets the sign bit of data - 'A', which determines if
-		// data is less than 
-		return (char)(data - (32 * ((data - 'A') >> 31)));
+		// gets the sign bit of 'Z' - data
+		// if data is greater than 'Z' then it will return -1
+		return (char)(data + (lowerUpperGap * (('Z' - data) >> 15)));
 	}
 
 }
